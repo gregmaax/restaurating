051c40d886fc25ea8533836bdcbee6ad93b7d40b
gregmaax <greg.d.972@gmail.com>
2024-05-24 17:11:36 +0200

    initial config

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,16 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { ButtonModule } from 'primeng/button';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, ButtonModule],
   template: `
-    <h1>Welcome to {{title}}!</h1>
+    <h1 class="text-amber-700 underline text-5xl m-3">
+      Welcome to {{ title }}!
+    </h1>
+    <p-button label="Clique ici" size="small" class="m-3" />
 
     <router-outlet />
   `,
